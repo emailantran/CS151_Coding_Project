b19646e004c4730322c15c0ffab8e8634f62b4cd
Andrew Vu <andrewkvu2@gmail.com>
2021-10-17 03:02:51 -0700

    made method stubs for wall class

--- src/Wall.java
+++ src/Wall.java
@@ -1,4 +1,51 @@
 
 public class Wall {
-
+	private int xPos;
+	private int yPos;
+	private int width;
+	private int height;
+	
+	
+	/*
+	 * Constructor for Wall Class
+	 */
+	public Wall(int xPos, int yPos, int width, int height) {
+		
+	}
+	
+	/*
+	 * Function to increase the xPos across the screen to create the illusion of flying through the walls
+	 */
+	public void wallMovement() {
+		
+	}
+	
+	/*
+	 * @return xPos 
+	 */
+	public int getxPos() {
+		
+	}
+	
+	/*
+	 * @return yPos
+	 */
+	public int getyPos() {
+		
+	}
+	
+	/*
+	 * @return width 
+	 */
+	public int getWidth() {
+		
+	}
+	
+	/*
+	 * @return height
+	 */
+	public int getHeight() {
+		
+	}
 }
+
