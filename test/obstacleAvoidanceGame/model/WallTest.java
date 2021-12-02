d5290cdc6ffbc868e2541eea57ca819f023e989b
anntran <emailantran@gmail.com>
2021-12-01 21:21:57 -0800

    Added unit test for wall and player model class

--- test/obstacleAvoidanceGame/model/WallTest.java
+++ test/obstacleAvoidanceGame/model/WallTest.java
@@ -7,8 +7,17 @@ import org.junit.jupiter.api.Test;
 class WallTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    void testMove() {
+        Wall testWall = new Wall(30);
+        testWall.move();
+        assertEquals(testWall.getX(), 1946, "Moves the wall over 4 left from the original 1950 so it should equal 1946");
+        testWall.setSpeed(2);
+        testWall.move();
+        assertEquals(testWall.getX(), 1944, "Moves the wall over 2 left from the previous 1946 so it should equal 1944");
+        for(int i = 0; i < 100; i++){
+            testWall.move();
+        }
+        assertEquals(testWall.getX(), 1744, "Moves the wall over 2 left 100 times from the previous 1944 so it should equal 1944");
+    }
 
 }
